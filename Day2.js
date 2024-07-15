1d76c0bfe531bdea08618419d04f4506dcd33311
Vipul20012001 <59278273+Vipul20012001@users.noreply.github.com>
2024-07-15 22:19:15 +0530

    Add files via upload

---
+++ Day2.js
@@ -0,0 +1,46 @@
+var a=1;
+var b=2;
+
+//WAP to add two number and log the result to console
+console.log(a+b);
+
+//WAP to substract two number and log the result to the console 
+ console.log(b-a);
+
+ //WAP to multiply two number and log the result
+ console.log(a*b);
+
+ //WAP to divide two numer and log the result to the console
+ console.log(a/b);
+ 
+ //WAP to find the remainder whent on e is divided and cosole log the result 
+ console.log(a%b);
+
+ // using += assingment operator 
+ a+=1;
+ console.log(a);
+ //WAP to -= assignment operator 
+ b-=1;
+ console.log(b);
+
+ //WAP comparing the number > and < and lof the result
+ console.log(a<b);
+ console.log(b>a);
+ console.log(a>=b);
+ console.log(b<=a);
+
+ //checking the value
+ console.log(a==b);
+
+ //checking the value with type
+ console.log(a===b);
+
+ // WAP to using logical operator 
+ console.log(1&&0);
+ console.log(true || false);
+ console.log(!true);
+
+ //Ternary Operator
+console.log(a>=b? true : false );
+
+
---
+++ Days1.js
@@ -0,0 +1,36 @@
+// using var keyword
+var age=10;
+console.log(age);
+
+// using let keyword
+let name = 'Vipul';
+console.log(name);
+
+// using const keyword
+const hide=true;
+console.log(hide);
+
+
+//using typer of and creating array and object 
+let person = new Object();
+person.name='Vipul';
+person.age='23';
+person.edu='Gradutaion';
+cars = ["Mahindra", "Tata", "Suzuki"];
+console.log(typeof(person));
+console.log(typeof(cars));
+console.log(typeof(name));
+console.log(typeof(age));
+
+
+//reassing the value of variable made by using let 
+let test="I'm testing"
+console.log(test)
+test="I'm done testing"
+console.log(test)
+
+// changing the value of the variable declared using const 
+const jo=1
+console.log(jo)
+jo=2
+console.log(jo)
